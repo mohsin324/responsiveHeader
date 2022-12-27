1aa54fdee236b2a9e6d052c644afe47fc17cb49f
Mohsin Munir <mohsinmunir324@gmail.com>
2022-12-27 17:59:59 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// const mobile_nav = document.querySelector('.mobile-navbar-btn');
+// const header_nav = document.querySelector('.header');
+
+// const toggleNavbar = ()=> {
+//     // alert('dfkjas')
+//     header_nav.classList.add = 'active'
+// }
+// mobile_nav.addEventListener('click', ()=> toggleNavbar())
+
+// const assignTEmp = ['7890'];
+// // const b = '56789'
+// const newArray  = [];
+
+// newArray.push(assignTEmp)
+// console.log(newArray)
+
+const templateCode = ['56789']
+// console.log(arrayTemplateCode.toString())
+
+// const templateCode = '34567'
+
+const newArray = [];
+
+newArray.push(templateCode)
+console.log(newArray[0].toString())
+
+
+// let templateCode = '56789'
+// let templateCode = ['5678']
+// if(templateCode[0]){
+//     console.log(templateCode)
+// }

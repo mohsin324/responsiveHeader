f51e899ce8033e6f0dacf12171379f60f21c2c04
Mohsin Munir <mohsinmunir324@gmail.com>
2023-01-03 22:46:34 +0500

    complete responsive header

--- script.js
+++ script.js
@@ -1,32 +1,12 @@
-// const mobile_nav = document.querySelector('.mobile-navbar-btn');
-// const header_nav = document.querySelector('.header');
+// Getting DOM element
+const icon = document.querySelector('.icon');
+const header = document.querySelector('.header');
 
-// const toggleNavbar = ()=> {
-//     // alert('dfkjas')
-//     header_nav.classList.add = 'active'
-// }
-// mobile_nav.addEventListener('click', ()=> toggleNavbar())
+function addClass(){
+    // navBar.setAttribute('class', 'active');
+    // header.setAttribute('class', 'active');
+    header.classList.toggle('active')
 
-// const assignTEmp = ['7890'];
-// // const b = '56789'
-// const newArray  = [];
+}
+icon.addEventListener('click', addClass);
 
-// newArray.push(assignTEmp)
-// console.log(newArray)
-
-const templateCode = ['56789']
-// console.log(arrayTemplateCode.toString())
-
-// const templateCode = '34567'
-
-const newArray = [];
-
-newArray.push(templateCode)
-console.log(newArray[0].toString())
-
-
-// let templateCode = '56789'
-// let templateCode = ['5678']
-// if(templateCode[0]){
-//     console.log(templateCode)
-// }
